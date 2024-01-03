182eadb3f6fc408703e314b5e74b621cf2eb6434
gravitys <alternativegravitational@protonmail.com>
2024-01-03 18:10:07 +0800

    uuuh

--- scripts/asteroid.gd
+++ scripts/asteroid.gd
@@ -7,17 +7,18 @@ var rock_damage = randi_range(1, 3)
 
 
 func _ready():
-	$TileMap.set_layer_modulate(
-		0, Color(1, float(1) / rock_damage, float(1) / rock_damage, 1)
-	)
 	$TileMap.set_cell(
 		0, Vector2i(0, 0), 0, Vector2i(randi_range(0, 3), randi_range(0, 2))
 	)
 
 
 func _process(delta):
-	rotation_degrees += rotation_speed * delta
-	position += speed * direction * delta
+	if speed != 0:
+		$TileMap.set_layer_modulate(
+			0, Color(1, float(1) / rock_damage, float(1) / rock_damage, 1)
+		)
+		rotation_degrees += rotation_speed * delta
+		position += speed * direction * delta
 
 
 func _on_body_entered(body: Node2D):
--- scripts/background.gd
+++ scripts/background.gd
@@ -1,5 +1,6 @@
 extends TileMap
 
+## MUST BE SET
 ## Use any node as an anchor where the background will use the
 ## anchor.global_position as a CENTER for the background that will be rendered.
 @export var background_anchor_point: Node
@@ -12,57 +13,77 @@ extends TileMap
 var width: int = size * 2
 var height: int = width
 
+## When enabled, will repleace the standard FastNoiseLite with a randomized pattern instead
 ## Changes the chance to spawn from the stars_tileset instead of the black tileset
 ## 1.0 = 1%, 60.0 = 60% chance
-@export_range(0.0, 100.0) var starryness: float = 1.0
+@export_range(0.0, 100.0) var starryness: float
+
+var stars = FastNoiseLite.new()
 
 
 func _ready():
+	assert(
+		background_anchor_point,
+		"Must define background_anchor_point as a node that is exists and is not a child of background"
+	)
+
+	stars.seed = randi()
 	if one_shot:
-		generate_map(
-			Vector2i(
-				(
-					(background_anchor_point.position / size)
-					- Vector2(size, size)
-				)
-			)
-		)
+		generate_map(background_anchor_point)
 
 
 func _process(_delta):
 	if not one_shot:
-		generate_map(
-			Vector2i(
-				(
-					(background_anchor_point.position / size)
-					- Vector2(size, size)
-				)
-			)
-		)
+		generate_map(background_anchor_point)
 
 
 ## generate a tile based on pos
-func generate_map(pos):
+func generate_map(anchor):
+	var pos = local_to_map(anchor.position)
+
 	for x in range(width):
 		for y in range(height):
+			var x_tile = (pos.x - (float(width) / 2)) + x
+			var y_tile = (pos.y - (float(height) / 2)) + y
+			# print(x_tile, y_tile)
 			# This will run for every cell
 
 			# This if statement checks if the current cell is already set
 			# And will run the return if it already does
-			if get_cell_source_id(0, Vector2i(x, y) + pos, false) != -1:
+			if get_cell_source_id(0, Vector2i(x_tile, y_tile), true) != -1:
 				continue
 
-			# Default atlas_id = 0
-			# Default atlas_coords = 0, 0
 			var atlas_id = 0
-			var atlas_coords = Vector2i(0, 0)
+			var atlas_coords = Vector2(0, 0)
+			var alternative_tile = 0
 
-			# The staryness percent chance is correct
-			if randf_range(0, 100) < starryness:
-				# Use the second atlas
-				atlas_id = 1
-				# Randomly select an atlas coordinate from 0, 1, 2 horizontally and vertically
-				atlas_coords = Vector2i(randi_range(0, 2), randi_range(0, 2))
+			# Check if starryness is defined
+			if starryness:
+				# The staryness percent chance is correct
+
+				if randf_range(0, 100) < starryness:
+					# Use the second atlas
+					atlas_id = 1
+					# Randomly select an atlas coordinate from 0, 1, 2 horizontally and vertically
+					atlas_coords = Vector2i(
+						randi_range(0, 2), randi_range(0, 2)
+					)
+			else:
+				# Use noise instead
+				atlas_coords = Vector2i(
+					round(
+						((stars.get_noise_2d(x_tile, y_tile) * 10) + 20) / 5
+					),
+					0
+				)
+				alternative_tile = randi_range(0, 3)
+				atlas_id = 2
 
 			# Set cell's tile from atlas_id and atlas_coords
-			set_cell(0, Vector2i(x, y) + pos, atlas_id, atlas_coords)
+			set_cell(
+				0,
+				Vector2i(x_tile, y_tile),
+				atlas_id,
+				atlas_coords,
+				alternative_tile
+			)
